b088caaae99a06509469a81aa80b0b7a8633006c
Santhoshkumargaja <gajasanthoshkumar@gmail.com>
2022-02-09 13:42:40 +0530

    Updated

---
+++ kk/ass/ass/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more informati
+import os
+path=os.chdir("C:\Users\gajas\Downloads\files")
+
+i = 0
+for file in os.listdir(path):
+    newfilename = "file{}.pdf".format(i)
+    os.rename(file, newfilename)
+    i = i + 1
